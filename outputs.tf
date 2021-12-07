839a15f4cd4bfc92b076556f4fd5fbd9d0c188b1
Sebastian Macarescu <sebastian.macarescu@assist.ro>
2021-12-07 14:08:05 +0200

    add fields_json output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "merged" {
   description = "The merged map."
   value       = local.m0
 }
+
+output "fields_json" {
+  description = "For each input map, convert it to a single-level map with a unique key for every nested value"
+  value = local.fields_json
+}
